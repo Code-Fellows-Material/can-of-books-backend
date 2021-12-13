8896d2eac2825ed36b4d6c28158a13a528cdc7d8
Kellen-Linse <kblinse06@gmail.com>
2021-12-13 15:13:56 -0800

    adds book model

--- server.js
+++ server.js
@@ -3,11 +3,24 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
 
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
+
+mongoose.connect(process.env.DB_LOCAL_URL);
+
+const db = mongoose.connection;
+// on the event of an error, do this:
+db.on('error', console.error.bind(console, 'connection error:'));
+// once db open, notify user via console.log
+db.once('open', function() {
+console.log('Mongoose is connected')
+});
+
 
 app.get('/test', (request, response) => {
 
