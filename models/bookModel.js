aed708bbf18d9d04c017c3ad87ea84f00fe71efd
Kellen-Linse <kblinse06@gmail.com>
2021-12-13 16:11:43 -0800

    adds GET request

--- models/bookModel.js
+++ models/bookModel.js
@@ -6,10 +6,10 @@ const {Schema} = mongoose;
 // title, desc, status, email
 
 const bookSchema = new Schema({
-  title = String,
-  description = String,
-  status = String,
-  email = String
+  title: String,
+  description: String,
+  status: String,
+  email: String
 })
 
 const Book = mongoose.model('Book', bookSchema);
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const Book = require('./models/bookModel');
 
 
 const app = express();
@@ -23,9 +24,26 @@ console.log('Mongoose is connected')
 
 
 app.get('/test', (request, response) => {
-
   response.send('test request received')
-
 })
+
+app.get('/books', handleGetBooks);
+
+async function handleGetBooks(req, res) {
+  let userFromClient = {};
+  if (req.query.user) {
+    userFromClient = { email: req.query.user }
+  }
+  try {
+    const booksFromDB = await Book.find(userFromClient);
+    if (booksFromDB.length > 0) {
+      res.status(200).send(booksFromDB);
+    } else {
+      res.status(404).send('No books found');
+    }
+  } catch(e) {
+    res.status(500).send('Server Error');
+  }
+};
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
